b084f0bd4758f6d577c01460370a205f23c67e4d
Rik Lomas <lomas.rik@gmail.com>
2019-10-01 17:29:36 -0400

    Update contentful.js

--- addon/adapters/contentful.js
+++ addon/adapters/contentful.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 import config from 'ember-get-config';
-import ajax from 'ember-fetch/ajax'
+import fetch from 'ember-fetch';
 
 export default DS.Adapter.extend({
   /**
@@ -185,7 +185,7 @@ export default DS.Adapter.extend({
       space
     } = this._getConfig();
 
-    return ajax(`https://${api}.contentful.com/spaces/${space}/${type}/${this._serializeQueryParams(data)}`, {
+    return fetch(`https://${api}.contentful.com/spaces/${space}/${type}/${this._serializeQueryParams(data)}`, {
       headers: {
         'Accept': 'application/json; charset=utf-8',
         'Authorization': `Bearer ${accessToken}`
